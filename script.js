aacae3cbb32f4d5aa9203adf475c7e402e73a2ed
Tristan Alie <TristanAlie@yahoo.com>
2022-07-11 22:36:03 -0400

    forgot to save before the previous commit. oops

--- script.js
+++ script.js
@@ -7,25 +7,27 @@ const canvasContext = canvas.getContext('2d')
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
-//images for sprites
+//images for sprites -- only one exists now
 const images = {}
 images.player = new Image()
 images.player.src = 'cuphead.png'
 
 //the character info
-const characterActions = ['up', 'up right', 'right', 'down right', 'down', 'jump']
+const characterActions = ['up', 'up right', 'right', 'down right', 'down', 'jump'] // the different things the character can do -- the png image has animations that i don't have actions for
 const numberOfCharacters = 30
 const characters = []
 class Character { // rewriting the character values as a class variable
     constructor(){
+        //the height and width of the character are based on the sprite image -- DON'T CHANGE!!!!!
         this.width = 103.0625
         this.height = 113.125
-        this.frameX = 3
+        this.frameX = 3 // this will get changed constantly, the starting value doesn't matter
         this.x = Math.random() * canvas.width - this.width;
         this.y = Math.random() * canvas.height - this.height;
         this.speed = (Math.random() * 5.5) + 3.5
         this.action = characterActions[Math.floor(Math.random() * characterActions.length)] // picks a random character action from the list
-        if(this.action === 'up'){
+        // the values set by the character's actions
+        if(this.action === 'up'){1
             this.frameY = 0
             this.minFrame = 4
             this.maxFrame = 15
@@ -51,12 +53,12 @@ class Character { // rewriting the character values as a class variable
             this.maxFrame = 12
         }
     }
-    draw() {
+    draw() { // the method that draws the the movement of the sprite
         drawSprite(images.player, this.width * this.frameX, this.height * this.frameY, this.width, this.height, this.x, this.y, this.width, this.height)
         if(this.frameX < this.maxFrame) this.frameX++
         else this.frameX = this.minFrame
     }
-    update() {
+    update() { // the method that updates the data of the sprite
         //move player
         if(this.action === 'right') {
             if(this.x > canvas.width + this.width) {
@@ -107,19 +109,21 @@ for(let i = 0; i < numberOfCharacters; i++) {
 function drawSprite(img, sX, sY, sWidth, sHeight, dX, dY, dWidth, dHeight) { //s is sources image, d is destination
     canvasContext.drawImage(img, sX, sY, sWidth, sHeight, dX, dY, dWidth, dHeight)
 }
-function animate(){
-    canvasContext.clearRect(0, 0, canvas.width, canvas.height)
-    for(let i = 0; i < characters.length; i++) {
+function animate(){ // renders every frame
+    canvasContext.clearRect(0, 0, canvas.width, canvas.height) // clear the canvas
+    for(let i = 0; i < characters.length; i++) { // display the characters with their updating values
         characters[i].draw();
         characters[i].update();
     }
     
 }
 
-console.log(characters)
-window.onload = setInterval(animate, 1000/30)
+console.log(characters) // just so i can view the characters and their values 
 
-window.addEventListener('resize', function(){
+// what causes the whole program to run
+window.onload = setInterval(animate, 1000/30) // 1000 is a second and i'm making 30 fps
+
+window.addEventListener('resize', function(){ // changes the canvas when the window size is changed
     canvas.width = window.innerWidth
     canvas.height = window.innerHeight
 })
