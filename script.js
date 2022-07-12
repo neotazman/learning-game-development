642b302aa1e61f7d72ef22c8156843d6c1ed4c88
Tristan Alie <TristanAlie@yahoo.com>
2022-07-11 20:23:09 -0400

    created code for more character actions

--- script.js
+++ script.js
@@ -13,36 +13,59 @@ images.player = new Image()
 images.player.src = 'cuphead.png'
 
 //the character info
-const characterActions = ['up', 'up right', 'right', 'down right', 'down', 'jump']
+const characterActions = ['up', 'right', 'jump']
+const numberOfCharacters = 10
 const characters = []
 class Character { // rewriting the character values as a class variable
     constructor(){
         this.width = 103.0625
         this.height = 113.125
         this.frameX = 3
-        this.frameY = 3
-        this.x = 0
-        this.y = 0
+        this.x = Math.random() * canvas.width - this.width;
+        this.y = Math.random() * canvas.height - this.height;
         this.speed = (Math.random() * 1.5) + 3.5
-        this.action = 'right'
+        this.action = characterActions[Math.floor(Math.random() * characterActions.length)] // picks a random character action from the list
+        if(this.action === 'up'){
+            this.frameY = 0
+        } else if(this.action === 'right') {
+            this.frameY = 3
+        } else if(this.action === 'jump') {
+            this.frameY = 7
+        }
     }
     draw() {
         drawSprite(images.player, this.width * this.frameX, this.height * this.frameY, this.width, this.height, this.x, this.y, this.width, this.height)
         //animate the sprites
         if(this.frameX < 13) this.frameX++
-        else this.frameX = 4
+        else this.frameX = 3
     }
     update() {
         //move player
         if(this.action === 'right') {
-            if(this.x < canvas.width + this.width) this.x+= this.speed
-            else this.x = 0 - this.width
+            if(this.x > canvas.width + this.width) {
+                this.x = 0 - this.width
+                this.y = Math.random() * (canvas.height - this.height)                 
+            }
+            else {
+                this.x+= this.speed
+            } 
+        } else if (this.action === 'up') {
+            if(this.y < (0 - this.height)) {
+                this.y = canvas.height + this.height
+                this.x = Math.random() * canvas.width
+            } else {
+                this.y -= this.speed
+            }
         }
-
     }
 }
-characters.push(new Character())
+// creating multiple characters
+for(let i = 0; i < numberOfCharacters; i++) {
+    characters.push(new Character())
+}
 
+
+console.log(characters)
 
 
 function drawSprite(img, sX, sY, sWidth, sHeight, dX, dY, dWidth, dHeight) { //s is sources image, d is destination
@@ -50,8 +73,11 @@ function drawSprite(img, sX, sY, sWidth, sHeight, dX, dY, dWidth, dHeight) { //s
 }
 function animate(){
     canvasContext.clearRect(0, 0, canvas.width, canvas.height)
-    characters[0].draw()
-    characters[0].update()
+    for(let i = 0; i < characters.length; i++) {
+        characters[i].draw();
+        characters[i].update();
+    }
+    
 }
 
 window.onload = setInterval(animate, 1000/30)
@@ -60,5 +86,3 @@ window.addEventListener('resize', function(){
     canvas.width = window.innerWidth
     canvas.height = window.innerHeight
 })
-
-console.log(images.player)
