ec7d12ba5d807234efa6a26fb3cae613b22da73a
Tristan Alie <TristanAlie@yahoo.com>
2022-07-11 21:18:14 -0400

    finished the character actions by myself woohoogit add .!

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ images.player = new Image()
 images.player.src = 'cuphead.png'
 
 //the character info
-const characterActions = ['up', 'right', 'jump']
+const characterActions = ['up', 'up right', 'right', 'down right', 'down', 'jump']
 const numberOfCharacters = 10
 const characters = []
 class Character { // rewriting the character values as a class variable
@@ -23,21 +23,38 @@ class Character { // rewriting the character values as a class variable
         this.frameX = 3
         this.x = Math.random() * canvas.width - this.width;
         this.y = Math.random() * canvas.height - this.height;
-        this.speed = (Math.random() * 1.5) + 3.5
+        this.speed = (Math.random() * 5.5) + 3.5
         this.action = characterActions[Math.floor(Math.random() * characterActions.length)] // picks a random character action from the list
         if(this.action === 'up'){
             this.frameY = 0
+            this.minFrame = 4
+            this.maxFrame = 15
         } else if(this.action === 'right') {
             this.frameY = 3
+            this.minFrame = 3
+            this.maxFrame = 13
         } else if(this.action === 'jump') {
             this.frameY = 7
+            this.minFrame = 0
+            this.maxFrame = 9
+        } else if(this.action === 'up right') {
+            this.frameY = 1
+            this.minFrame = 2
+            this.maxFrame = 14
+        } else if(this.action === 'down right') {
+            this.frameY = 4
+            this.minFrame = 4
+            this.maxFrame = 15
+        } else if(this.action === 'down') {
+            this.frameY = 6
+            this.minFrame = 0
+            this.maxFrame = 12
         }
     }
     draw() {
         drawSprite(images.player, this.width * this.frameX, this.height * this.frameY, this.width, this.height, this.x, this.y, this.width, this.height)
-        //animate the sprites
-        if(this.frameX < 13) this.frameX++
-        else this.frameX = 3
+        if(this.frameX < this.maxFrame) this.frameX++
+        else this.frameX = this.minFrame
     }
     update() {
         //move player
@@ -56,6 +73,29 @@ class Character { // rewriting the character values as a class variable
             } else {
                 this.y -= this.speed
             }
+        } else if(this.action === 'down') {
+            if(this.y > (canvas.height + this.height)) {
+                this.y = 0 - this.height
+                this.x = Math.random() * canvas.width
+            } else {
+                this.y+= this.speed
+            }
+        } else if(this.action === 'down right') {
+            if(this.y > canvas.height + this.height && this.x > canvas.width + this.width) {
+                this.y = 0 - this.height
+                this.x = Math.random() * canvas.width/2
+            } else {
+                this.y += this.speed
+                this.x += this.speed
+            }
+        } else if(this.action === 'up right') {
+            if(this.y < (0 - this.height) && this.x > canvas.width + this.width) {
+                this.y = canvas.height + this.height
+                this.x = Math.random() * canvas.width/2
+            } else {
+                this.y -= this.speed
+                this.x += this.speed
+            }
         }
     }
 }
@@ -63,10 +103,6 @@ class Character { // rewriting the character values as a class variable
 for(let i = 0; i < numberOfCharacters; i++) {
     characters.push(new Character())
 }
-
-
-console.log(characters)
-
 
 function drawSprite(img, sX, sY, sWidth, sHeight, dX, dY, dWidth, dHeight) { //s is sources image, d is destination
     canvasContext.drawImage(img, sX, sY, sWidth, sHeight, dX, dY, dWidth, dHeight)
@@ -80,6 +116,7 @@ function animate(){
     
 }
 
+console.log(characters)
 window.onload = setInterval(animate, 1000/30)
 
 window.addEventListener('resize', function(){
